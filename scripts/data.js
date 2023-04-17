f354c18a066f9afc88eedbd8cc7cd253a2220b28
Santiago Castro <s.castrod@uniandes.edu.co>
2023-04-16 22:06:29 -0500

    Intento #2

---
+++ scripts/data.js
@@ -0,0 +1,15 @@
+import { Serie } from "./serie.js";
+export var series = [
+    
+    new Serie(1,'Game of Thrones', 'HBO', 'Drama', 8, 73, 'La lucha por el Trono de Hierro enfrenta a las familias nobles de los Siete Reinos'),
+    new Serie(2,'Breaking Bad', 'Drama', 'AMC', 5, 62, 'Un profesor de química se convierte en un fabricante de drogas para asegurar el futuro financiero de su familia'),
+    new Serie(3,'The Big Bang Theory', 'CBS', 'Comedia', 12, 279, 'Las vidas de un grupo de amigos nerds cambian cuando una chica se muda al departamento de al lado'),
+    new Serie(4,'Friends', 'NBC', 'Comedia', 10, 236, 'Las aventuras y desventuras de seis amigos en Nueva York'),
+    new Serie(5,'Stranger Things', 'Ciencia ficción', 3, 25, 'Un niño desaparece misteriosamente en la década de los 80 en Indiana'),
+    new Serie(6,'The Crown', 'NETFLIX', 'Drama', 4, 40, 'La historia de la Reina Isabel II desde su boda en 1947 hasta la actualidad'),
+    new Serie(7,'Narcos', 'NETFLIX', 'Drama', 3, 30, 'La historia del ascenso y caída del cartel de drogas de Medellín'),
+    new Serie(8,'Black Mirror', 'NETFLIX', 'Ciencia ficción', 5, 22, 'Reflexiones sobre la tecnología y su impacto en la sociedad'),
+    new Serie(9,'Peaky Blinders', 'BBC', 'Drama', 5, 30, 'La historia de la familia Shelby y su banda de gánsteres en Birmingham después de la Primera Guerra Mundial'),
+    new Serie(10,'The Mandalorian', 'DISNEY+', 'Ciencia ficción', 2, 16, 'Un cazarrecompensas protege a un niño de la raza de Yoda en los confines de la galaxia')
+    
+  ];
